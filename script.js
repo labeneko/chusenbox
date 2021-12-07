4c8d59d33de3e83fe400ff9b693cf8fa9dc06369
labeneko <info@laboneko.jp>
2021-12-07 17:56:23 +0900

    課金機能を追加

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 $("#content").hide();
 
+let maxSec = 1000;
+let chosenColor = "#44A";
+let amount = 0;
+
+
 function choose() {
   const choosen = datas[Math.floor(Math.random() * datas.length)];
   $("#id").text(choosen.id);
@@ -12,14 +17,14 @@ function choose() {
 }
 
 function roll(sec) {
-  if (sec < 1000) {
+  if (sec < maxSec) {
     setTimeout(() => {
       choose();
       roll(sec * 1.08);
     }, sec < 10 ? 10 : sec);
   } else {
-    $("body").css("color", `#44A`);
-    $("#title").css("background-color", `#44A`);
+    $("body").css("color", chosenColor);
+    $("#title").css("background-color", chosenColor);
     $("#choose").prop('disabled', false);
     $("#play").show();
   }
@@ -34,4 +39,37 @@ $("#choose").on("click", () => {
   $("#choose").prop('disabled', true);
   $("#title").css("background-color", `#444`);
   roll(0.5);
+});
+
+
+$("#kakin_menu").on("click", () => {
+  $("#kakin_box").show();
+});
+
+$("#kakin").on("click", () => {
+  $("#kakin_box").hide();
+  maxSec = maxSec / 5;
+  amount += 10000;
+  $("#kakin_progress").show();
+  $("#amount").text(amount);
+  if (amount >= 10000) {
+    chosenColor = "darkred";
+  }
+  if (amount >= 20000) {
+    $("#choose").css("background",  "linear-gradient(-45deg, #F7DE05, #DA8E00, #EDAC06, #F7DE05, #ECB802, #DAAF08, #B67B03, #DA8E00, #EDAC06, #F7DE05, #ECB802, #EDAC06)");
+    $("#kakin_progress").css("color",  "#DA8E00");
+    $("#choose").css("border", "none");
+    $("#choose").css("color", "white");
+    $("#play").css("background",  "linear-gradient(-45deg, #F7DE05, #DA8E00, #EDAC06, #F7DE05, #ECB802, #DAAF08, #B67B03, #DA8E00, #EDAC06, #F7DE05, #ECB802, #EDAC06)").css("font-size",  "30px");
+    $("#play").css("border", "none");
+  }
+  if (amount >= 30000) {
+    $("#choose").css("background",  "linear-gradient(to right,red,orange,yellow,green,aqua,blue,purple)");
+    $("#choose").css("border", "none");
+    $("#play").css("background",  "linear-gradient(to right,red,orange,yellow,green,aqua,blue,purple)").css("font-size",  "30px");
+  }
+  if (amount >= 40000) {
+    $("#choose").css("font-size", "100px");
+    $("#play").css("font-size",  "60px");
+  }
 });
